ae8a5a262b3ac34c9e0983be44afa0c2fe46aae5
oliver.ferrigni <oliver.ferrigni@asolutions.com>
2011-10-26 23:04:52 -0500

    Added a game instance and a supervisor, untested.
    Oliver

---
+++ src/uno_game.erl
@@ -0,0 +1,145 @@
+%%%-------------------------------------------------------------------
+%%% @author oferrigni
+%%% @copyright (C) 2011, oferrigni
+%%% @doc
+%%%
+%%% @end
+%%% Created : 2011-10-26 22:44:05.799142
+%%%-------------------------------------------------------------------
+-module(uno_game).
+
+-behaviour(gen_server).
+
+%% API
+-export([start_link/0, hello_amos/0, get_arg/0]).
+
+%% gen_server callbacks
+-export([init/1,
+         handle_call/3,
+         handle_cast/2,
+         handle_info/2,
+         terminate/2,
+         code_change/3]).
+
+-define(SERVER, ?MODULE).
+
+-record(state, {last_arg}).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts the server
+%%
+%% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
+%% @end
+%%--------------------------------------------------------------------
+start_link() ->
+        gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+hello_amos() ->
+        gen_server:cast(?SERVER, {hello_amos, some_arg}).
+get_arg() ->
+        gen_server:call(?SERVER, get_arg).
+
+%%%===================================================================
+%%% gen_server callbacks
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Initializes the server
+%%
+%% @spec init(Args) -> {ok, State} |
+%%                     {ok, State, Timeout} |
+%%                     ignore |
+%%                     {stop, Reason}
+%% @end
+%%--------------------------------------------------------------------
+init([]) ->
+        {ok, #state{last_arg = "Empty"}}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling call messages
+%%
+%% @spec handle_call(Request, From, State) ->
+%%                                   {reply, Reply, State} |
+%%                                   {reply, Reply, State, Timeout} |
+%%                                   {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, Reply, State} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_call(get_arg, _From, #state{last_arg = LastArg} = State) ->
+        Reply = LastArg,
+        {reply, Reply, State};
+handle_call(_Request, _From, State) ->
+        Reply = ok,
+        {reply, Reply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling cast messages
+%%
+%% @spec handle_cast(Msg, State) -> {noreply, State} |
+%%                                  {noreply, State, Timeout} |
+%%                                  {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_cast({hello_amos, Arg}, _State) ->
+  {noreply, #state{last_arg = Arg}};
+handle_cast(_Msg, State) ->
+        {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling all non call/cast messages
+%%
+%% @spec handle_info(Info, State) -> {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_info(Info, State) ->
+  io:format("Received unknown message ~p~n", [Info]),
+        {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any
+%% necessary cleaning up. When it returns, the gen_server terminates
+%% with Reason. The return value is ignored.
+%%
+%% @spec terminate(Reason, State) -> void()
+%% @end
+%%--------------------------------------------------------------------
+terminate(_Reason, _State) ->
+        ok.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Convert process state when code is changed
+%%
+%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
+%% @end
+%%--------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+        {ok, State}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+
+
+
+
---
+++ src/uno_game_sup.erl
@@ -0,0 +1,73 @@
+%%%-------------------------------------------------------------------
+%%% @author oferrigni
+%%% @copyright (C) 2011, oferrigni
+%%% @doc
+%%%
+%%% @end
+%%% Created : 2011-10-26 22:42:40.892842
+%%%-------------------------------------------------------------------
+-module(uno_game_sup).
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+-define(SERVER, ?MODULE).
+
+%%%===================================================================
+%%% API functions
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts the supervisor
+%%
+%% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
+%% @end
+%%--------------------------------------------------------------------
+start_link() ->
+        supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+
+%%%===================================================================
+%%% Supervisor callbacks
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Whenever a supervisor is started using supervisor:start_link/[2,3],
+%% this function is called by the new process to find out about
+%% restart strategy, maximum restart frequency and child
+%% specifications.
+%%
+%% @spec init(Args) -> {ok, {SupFlags, [ChildSpec]}} |
+%%                     ignore |
+%%                     {error, Reason}
+%% @end
+%%--------------------------------------------------------------------
+init([]) ->
+        RestartStrategy = one_for_one,
+        MaxRestarts = 1000,
+        MaxSecondsBetweenRestarts = 3600,
+
+        SupFlags = {RestartStrategy, MaxRestarts, MaxSecondsBetweenRestarts},
+
+        Restart = permanent,
+        Shutdown = 2000,
+        Type = worker,
+
+        AChild = {'uno_game', {'uno_game', start_link, []},
+                          Restart, Shutdown, Type, ['uno_game']},
+
+        {ok, {SupFlags, [AChild]}}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+
+
+
