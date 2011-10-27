f920789d85542b944542953cbb2495ef55ff46a1
oliver.ferrigni <oliver.ferrigni@asolutions.com>
2011-10-26 22:41:55 -0500

    Established Child that is supervisor for the game.
    Oliver

--- test/uno_sup_test.erl
+++ test/uno_sup_test.erl
@@ -3,7 +3,12 @@
 -compile([export_all]).
 
 -include_lib("eunit/include/eunit.hrl").
+-define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
 
 init_should_return_one_for_one_restart_strategy_test() ->
   {_,{ RestartStrategy, _}} = uno_sup:init([]),
   ?assertMatch({one_for_one, 5, 10}, RestartStrategy).
+
+init_should_return_uno_game_supervisor_test() ->
+  {_, {_, Children}} = uno_sup:init([]),
+  ?assertMatch([?CHILD(uno_game_sup, supervisor)], Children).
