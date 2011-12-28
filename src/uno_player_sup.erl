8748125bbcab6d8ef02af5c3c21ec7cadf0c8078
dave.mccown <dave.mccown@asolutions.com>
2011-12-27 21:33:37 -0600

    Introduced player supervisor which will be a simple_one_for_one supervisor

---
+++ src/uno_player_sup.erl
@@ -0,0 +1,17 @@
+-module(uno_player_sup).
+
+-behaviour(supervisor).
+
+-export([start_link/0]).
+
+-export([init/1]).
+
+-include("include/uno_macros.hrl").
+
+-define(SERVER, ?MODULE).
+
+start_link() -> 
+        supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+
+    init([]) ->  {ok}.
+
--- test/uno_game_sup_test.erl
+++ test/uno_game_sup_test.erl
@@ -5,11 +5,11 @@
 -include("include/uno_macros.hrl").
 -include_lib("eunit/include/eunit.hrl").
 
-init_should_return_child_specifications_including_four_players_test() ->
-    {ok, {SupervisorFloags, Children}}  =  uno_game_sup:init([]),
-    ?assertMatch({one_for_one, 2, 3600}, SupervisorFloags),
+init_should_return_child_specifications_including_the_uno_game_and_uno_player_sup_test() ->
+    {ok, {SupervisorFlags, Children}}  =  uno_game_sup:init([]),
+    ?assertMatch({one_for_one, 2, 3600}, SupervisorFlags),
     ?assertMatch([_,_], Children),
-    [UnoGame, UnoPlayer] = Children,
+    [UnoGame, UnoPlayerSup] = Children,
     ?assertEqual(UnoGame, ?CHILD(uno_game, worker, 2000)),
-    ?assertEqual(UnoPlayer, ?CHILD(uno_player, worker, 2000)).
+    ?assertEqual(UnoPlayerSup, ?CHILD(uno_player_sup, supervisor, 2000)).
 
